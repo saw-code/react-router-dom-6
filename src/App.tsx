e5c35cf36fb77451cfc1052cc089fe66335bc680
saw-code <saw.coding@gmail.com>
2023-02-02 11:19:32 +0300

    3 lesson finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
+
+const Profile = () => {
+  const params = useParams()
+  // const params = useParams<'id'>() можно типизировать useParams и тогда у params можно вызывать эти ключи. Пример params.id
+  console.log(params)
+
+  return <div>profile</div>
+}
 
 function App() {
   return (
@@ -9,36 +17,16 @@ function App() {
       <NavLink to={'/'}>main</NavLink>---
       <NavLink to={'/login'}>login</NavLink>---
       <NavLink to={'/profile'}>profile</NavLink>---
-      <NavLink to={'/profile/settings'}>settings</NavLink>
+      <NavLink to={'/profile/1'}>profile/1</NavLink>
 
       <Routes>
         <Route path={'/*'} element={<div>404</div>} />    {/* звездочка говорит что после слеша может быть что угодно */}
         <Route path={'/'} element={<div>main</div>} />
-        <Route path={'/login/*'} element={<div>login</div>} /> {/* звездочка после слеша /login/*  означает что чтобы не было после login то останется все равно на вкладке login */}
-
-        {/*второй вариант вложенности*/}
-        <Route path={'/profile'} element={(
-          <div>
-            profile
-            <Outlet/> {/* вместо Outlet вставится роут profile/settings */}
-          </div>
-          )}>
-          <Route path={'*'} element={<div>profile page not found</div>} /> {/*если ввести /profile/settings с несуществующим именем, например /profile/settingskkkkk или /profile/settings/blabla то вместо элемента settings отрисуется profile page not found*/}
-          <Route index element={<div>checked id</div>} /> {/*index говорит о том, что у нас нет никаких параметров после /profile, т.е. если мы напишем /profile то отрисуется checked id*/}
-          <Route path={':id'} element={<div>id</div>} /> {/*Если вместо /profile/settings будет например /profile/blabla то отрисуется элемент id */}
-          <Route path={'settings'} element={<div>settings</div>} /> {/*можно просто settings и будет работать, а если /settings то будет ошибка*/}
-        </Route>
-
-        {/*первый вариант вложенности*/}
-        {/*<Route path={'/profile/*'} element={*/}
-        {/*  <div>*/}
-        {/*    profile*/}
-        {/*    <Routes>*/}
-        {/*      <Route path={'/settings'} element={<div>settings</div>} /> /!* вложили settings в profile и теперь на странице settings отрисуются оба элемента *!/*/}
-        {/*    </Routes>*/}
-        {/*  </div>*/}
-        {/*} />*/}
-        {/*<Route path={'/profile/settings'} element={<div>settings</div>} />*/}
+        <Route path={'/login'} element={<div>login</div>} />
+        <Route path={'/profile/:id'} element={<Profile/>} /> {/* передаем в element компоненту и с помощью useParams
+        присваивается ключ-значение ключом будет id а значением то что укажем после profile
+         например profile/blabla вернет объект { id: "blabla" } где значение всегда строка
+         */}
       </Routes>
     </div>
   );
