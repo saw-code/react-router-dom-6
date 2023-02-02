ad9123eb705bfed694d3eba3666a7e8553e61b0b
saw-code <saw.coding@gmail.com>
2023-02-02 13:22:27 +0300

    5 lesson finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes, useNavigate} from "react-router-dom";
+
+const Profile = () => {
+  const navigate = useNavigate() // с помощью этого хука мы получаем ф-цию и ей в дальнейшем передаем /login и переходим на эту страницу
+
+  // 1 вариант
+  // useEffect(() => {
+  //   if (true) navigate('/login')
+  // }, [])
+
+
+  return (
+    <div>
+      {/*2 вариант*/}
+    {/*  {true ? (*/}
+    {/*    <Navigate to={'/login'}/>*/}
+    {/*  ):(*/}
+    {/*    <>*/}
+    {/*      profile*/}
+    {/*      <button onClick={() => {navigate('/login')}}>logout</button>*/}
+    {/*    </>*/}
+    {/*)}*/}
+      <button onClick={() => {navigate(-1)}}>logout</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <NavLink to={'/'}>main</NavLink>---
       <NavLink to={'/login'}>login</NavLink>---
-
-
-      <NavLink
-        to={'/profile'}
-        style={(params) => {
-          return {color: params.isActive ? 'lime' : 'black'}
-        }}
-      >
-        profile
-      </NavLink>---
-
-      <NavLink
-        to={'/profile/settings'}
-        className={({isActive}) => isActive ? 'act' : 'def'}
-      >
-        settings
-      </NavLink>---
-
-      <a
-        href={'https://github.com/IgnatZakalinsky/simple-react-homeworks'}
-        target={'_blank'} // открывается в новой вкладке
-        rel={'noreferrer nofollow noopener'} // это для безопасности
-      >
-        xxx
-      </a>
+      <NavLink to={'/profile'}>profile</NavLink>---
+      <NavLink to={'/profile/settings'}>settings</NavLink>
 
       <Routes>
         <Route path={'/*'} element={<div>404</div>}/> {/* звездочка говорит что после слеша может быть что угодно */}
         <Route path={'/'} element={<div>main</div>}/>
         <Route path={'/login'} element={<div>login</div>}/>
-        <Route path={'/profile'} element={<div>profile</div>}/>
+        <Route path={'/profile'} element={<Profile/>}/>
         <Route path={'/profile/settings'} element={<div>settings</div>}/>
       </Routes>
     </div>
