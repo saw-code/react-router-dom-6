c7459023840bee2bcfc67b9507e54056fb34e339
saw-code <saw.coding@gmail.com>
2023-02-02 12:02:26 +0300

    4 lesson finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
-
-const Profile = () => {
-  const params = useParams()
-  // const params = useParams<'id'>() можно типизировать useParams и тогда у params можно вызывать эти ключи. Пример params.id
-  console.log(params)
-
-  return <div>profile</div>
-}
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavLink to={'/'}>main</NavLink>---
       <NavLink to={'/login'}>login</NavLink>---
-      <NavLink to={'/profile'}>profile</NavLink>---
-      <NavLink to={'/profile/1'}>profile/1</NavLink>
+
+
+      <NavLink
+        to={'/profile'}
+        style={(params) => {
+          return {color: params.isActive ? 'lime' : 'black'}
+        }}
+      >
+        profile
+      </NavLink>---
+
+      <NavLink
+        to={'/profile/settings'}
+        className={({isActive}) => isActive ? 'act' : 'def'}
+      >
+        settings
+      </NavLink>---
+
+      <a
+        href={'https://github.com/IgnatZakalinsky/simple-react-homeworks'}
+        target={'_blank'} // открывается в новой вкладке
+        rel={'noreferrer nofollow noopener'} // это для безопасности
+      >
+        xxx
+      </a>
 
       <Routes>
-        <Route path={'/*'} element={<div>404</div>} />    {/* звездочка говорит что после слеша может быть что угодно */}
-        <Route path={'/'} element={<div>main</div>} />
-        <Route path={'/login'} element={<div>login</div>} />
-        <Route path={'/profile/:id'} element={<Profile/>} /> {/* передаем в element компоненту и с помощью useParams
-        присваивается ключ-значение ключом будет id а значением то что укажем после profile
-         например profile/blabla вернет объект { id: "blabla" } где значение всегда строка
-         */}
+        <Route path={'/*'} element={<div>404</div>}/> {/* звездочка говорит что после слеша может быть что угодно */}
+        <Route path={'/'} element={<div>main</div>}/>
+        <Route path={'/login'} element={<div>login</div>}/>
+        <Route path={'/profile'} element={<div>profile</div>}/>
+        <Route path={'/profile/settings'} element={<div>settings</div>}/>
       </Routes>
     </div>
   );
